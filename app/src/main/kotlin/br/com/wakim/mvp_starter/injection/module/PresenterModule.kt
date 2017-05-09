7390dc266ebb669e3776806756d07d14d7e7d0c1
wakim <wakim.jraige@gmail.com>
2017-05-09 17:44:56 -0300

    Fix DataSource package import

--- app/src/main/kotlin/br/com/wakim/mvp_starter/injection/module/PresenterModule.kt
+++ app/src/main/kotlin/br/com/wakim/mvp_starter/injection/module/PresenterModule.kt
@@ -1,6 +1,6 @@
 package br.com.wakim.mvp_starter.injection.module
 
-import br.com.wakim.mvp_starter.data.remote.PostDataSource
+import br.com.wakim.mvp_starter.data.PostDataSource
 import br.com.wakim.mvp_starter.injection.ConfigPersistent
 import br.com.wakim.mvp_starter.ui.DefaultScheduler
 import br.com.wakim.mvp_starter.ui.posts_list.PostListContract
